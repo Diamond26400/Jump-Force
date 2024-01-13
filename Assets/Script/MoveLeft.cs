10f9a86c8b0b2b90f5f81b612d25f2b7588f4ba0
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-13 19:23:50 +0100

    Update MoveLeft.cs

--- Assets/Script/MoveLeft.cs
+++ Assets/Script/MoveLeft.cs
@@ -7,10 +7,7 @@ public class MoveLeft : MonoBehaviour
 {
      public  float speed = 5.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
